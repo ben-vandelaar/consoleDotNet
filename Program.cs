1356ce74e3c88cfca77f4b793b3c02dcae9aa291
Ben <32469338+ben-vandelaar@users.noreply.github.com>
2018-10-24 13:51:35 +1300

    uploaded files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consoleDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
